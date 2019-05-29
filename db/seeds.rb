bbc475f151e04b176c79b8521d05e8bdcce3bbb0
StephenReiser <Stephen.Reiser1@gmail.com>
2019-05-29 13:46:29 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+House.create([
+{
+    address: '5806 Caldwell Rd',
+    city: 'Loudon',
+    state: 'TN',
+    price: 115158,
+    favorite: false,
+    img: 'https://photos.zillowstatic.com/p_h/ISq9w8gip460mr0000000000.jpg',
+    url: 'https://www.zillow.com/homedetails/5806-Caldwell-Rd-Loudon-TN-37774/66932261_zpid/'
+    },{
+    address: '107 Fairway Dr',
+    city: 'Loudon', 
+    state: 'TN', 
+    price: 134291, 
+    favorite: false, 
+    img: 'https://photos.zillowstatic.com/p_h/ISahnq8vqaoou91000000000.jpg', 
+    url: 'https://www.zillow.com/homedetails/107-Fairway-Dr-Loudon-TN-37774/41743679_zpid/'
+    },{
+    address: '302 Valley Dr, Loudon',
+    city: 'Loudon', 
+    state: 'TN', 
+    price: 88731, 
+    favorite: false, 
+    img: 'https://photos.zillowstatic.com/p_h/IS1kiunf4vrzctf.jpg', 
+    url: 'https://www.zillow.com/homedetails/302-Valley-Dr-Loudon-TN-37774/90135343_zpid/'
+    },{
+    address: '220 Vineyard Cove Dr', 
+    city: 'Loudon', 
+    state: 'TN', 
+    price: 58885, 
+    favorite: false, 
+    img: 'https://photos.zillowstatic.com/p_h/IS9hfwf23wi1ka0000000000.jpg', 
+    url: 'https://www.zillow.com/homedetails/220-Vineyard-Cove-Dr-Loudon-TN-37774/66927830_zpid/'
+    },{
+    address: '2443 Stockton Valley Rd', 
+    city: 'Loudon', 
+    state: 'TN', 
+    price: 1100000, 
+    favorite: false, 
+    img: 'https://www.zillow.com/homedetails/302-Valley-Dr-Loudon-TN-37774/90135343_zpid/', 
+    url: 'https://www.zillow.com/homedetails/2443-Stockton-Valley-Rd-Loudon-TN-37774/66925967_zpid/'  }
+
+
+
+
+])
